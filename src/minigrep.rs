c3a7971e2279472c3a16d1e76ea21dd04561e992
ppdx999 <ppdx999@gmail.com>
2024-01-10 09:40:06 +0900

    :hammer: [minigrep] improve error handling

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -4,7 +4,10 @@ use std::io::prelude::*;
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -24,12 +27,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         // There will be another better way to do the same thing
         // without cloning the args
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
