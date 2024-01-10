2dbf6fe89ad4f5e293a70f2ddfec7221c2b21e71
ppdx999 <ppdx999@gmail.com>
2024-01-10 09:44:42 +0900

    :hammer: [minigrep] importを関連に従う

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -1,12 +1,13 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
+        process::exit(1);
     });
 
     println!("Searching for {}", config.query);
