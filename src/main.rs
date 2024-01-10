7336c71d2948418b1b2248e6ee9722c27706577b
ppdx999 <ppdx999@gmail.com>
2024-01-10 09:24:15 +0900

    [mingrep] ファイルを読み取りprintする

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // mod guessing_game;
-mod list_example;
+// mod list_example;
+mod minigrep;
 
 fn main() {
     // guessing_game::main();
-    list_example::main();
+    // list_example::main();
+    minigrep::main();
 }
