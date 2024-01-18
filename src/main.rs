2b98ffa8156d3234cfa90741652dac35d50f3d11
ppdx999 <ppdx999@gmail.com>
2024-01-18 09:47:02 +0900

    mpscのサンプルを追加

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 // mod guessing_game;
 // mod list_example;
-mod minigrep;
-mod mock;
+// mod minigrep;
+// mod mock;
+mod thread;
 
 fn main() {
     // guessing_game::main();
     // list_example::main();
-    minigrep::main();
+    // minigrep::main();
+    thread::single_message();
+    thread::multiple_message();
 }
---
+++ src/thread.rs
@@ -0,0 +1,34 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
+pub fn single_message() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+
+pub fn multiple_message() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread")
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
