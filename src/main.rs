cef424aefea32d5a68ba290baba1da17e3893b9e
ppdx999 <ppdx999@gmail.com>
2024-01-18 09:56:27 +0900

    mutexのサンプルを追加

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 // mod list_example;
 // mod minigrep;
 // mod mock;
-mod thread;
+// mod thread;
+mod mutex;
 
 fn main() {
     // guessing_game::main();
     // list_example::main();
     // minigrep::main();
-    thread::single_message();
-    thread::multiple_message();
+    // thread::single_message();
+    // thread::multiple_message();
+    mutex::main();
 }
