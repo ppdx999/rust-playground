bd4292a6795128ee8cb311a918486ab5cca3bbc3
ppdx999 <ppdx999@gmail.com>
2024-01-26 23:41:14 +0900

    oo_design_patternステートパターンの簡易的実装

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 // mod mock;
 // mod thread;
 // mod mutex;
-mod trait_objects;
+// mod trait_objects;
+mod oo_design_patterns;
 
 fn main() {
     // guessing_game::main();
@@ -13,5 +14,6 @@ fn main() {
     // thread::single_message();
     // thread::multiple_message();
     // mutex::main();
-    trait_objects::main();
+    // trait_objects::main();
+    oo_design_patterns::main();
 }
